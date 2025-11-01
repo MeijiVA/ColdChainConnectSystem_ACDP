e331e838292ca968d81c8a3cb11e467f7b0ea50b
MeijiVA <mizaelantonbautista@gmail.com>
2025-11-01 14:40:03 +0800

    Removed paint shadow

--- ClassResources/DisplayClass.cs
+++ ClassResources/DisplayClass.cs
@@ -18,41 +18,13 @@ namespace ColdChainConnectSystem_ACDP
         public abstract void SetDisplayCircle(PictureBox b);
         public abstract void SetDisplayRectangle(Label l, int arc1, int arc2, int arc3, int arc4);
         public abstract void SetDisplayRectangle(Panel p,int arc1,int arc2,int arc3,int arc4);
-        public abstract void Panel_Paint(object sender, PaintEventArgs e);
         public abstract void SetDisplayRectangle(TextBox t, int arc1, int arc2, int arc3, int arc4);
     }
 
 
     internal class DisplayClass : CColor
     {
-        public Panel myPanel;
-
-        public override void Panel_Paint(object sender, PaintEventArgs e)
-        {
-
-            if (myPanel != null)
-            {
-                Color[] shadowColors = new Color[5];
-                shadowColors[0] = Color.FromArgb(130, 130, 130); // Darkest shade
-                shadowColors[1] = Color.FromArgb(181, 181, 181); // Darker shade
-                shadowColors[2] = Color.FromArgb(195, 195, 195); // Medium shade
-                shadowColors[3] = Color.FromArgb(211, 211, 211); // Lighter shade
-                shadowColors[4] = Color.FromArgb(230, 230, 230); // Lightest shade
-                using (Pen pen = new Pen(shadowColors[0]))
-                {
-                    Point pt = myPanel.Location;
-                    pt.Y += myPanel.Height;
-
-                    for (int sp = 0; sp < 5; sp++)
-                    {
-                        pen.Color = shadowColors[sp];
-                        e.Graphics.DrawLine(pen, pt.X + sp, pt.Y, pt.X + myPanel.Width - 1 + sp, pt.Y); // Bottom shadow
-                        e.Graphics.DrawLine(pen, myPanel.Right + sp, myPanel.Top + sp, myPanel.Right + sp, myPanel.Bottom + sp); // Right shadow
-                        pt.Y++;
-                    }
-                }
-            }
-        }
+       
 
         public override Color SetCustomColor(string color)
         {
