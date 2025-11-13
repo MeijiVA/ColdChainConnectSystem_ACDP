3595919b73d3c07977cd22a94a13c15c78480f5a
dncdnl <danicadaniela.cesicar@gmail.com>
2025-11-13 15:05:19 +0800

    Added SearchUC and DisplayTableUC to the SalesForm

--- AppForms/MainPanel/Sales/SalesForm.Designer.cs
+++ AppForms/MainPanel/Sales/SalesForm.Designer.cs
@@ -28,12 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.searchUC1 = new ColdChainConnectSystem_ACDP.AppForms.MainPanel.Inventory.SearchUC();
+            this.displayTableUC1 = new ColdChainConnectSystem_ACDP.AppForms.MainPanel.Inventory.DisplayTableUC();
+            this.SuspendLayout();
+            // 
+            // searchUC1
+            // 
+            this.searchUC1.BackColor = System.Drawing.Color.Transparent;
+            this.searchUC1.Location = new System.Drawing.Point(21, 12);
+            this.searchUC1.Name = "searchUC1";
+            this.searchUC1.searchTXT = "Search Term";
+            this.searchUC1.Size = new System.Drawing.Size(755, 65);
+            this.searchUC1.TabIndex = 0;
+            // 
+            // displayTableUC1
+            // 
+            this.displayTableUC1.BackColor = System.Drawing.Color.White;
+            this.displayTableUC1.Location = new System.Drawing.Point(21, 94);
+            this.displayTableUC1.Name = "displayTableUC1";
+            this.displayTableUC1.Size = new System.Drawing.Size(755, 442);
+            this.displayTableUC1.TabIndex = 1;
+            // 
+            // SalesForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(798, 570);
+            this.Controls.Add(this.displayTableUC1);
+            this.Controls.Add(this.searchUC1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "SalesForm";
             this.Text = "SalesForm";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Inventory.SearchUC searchUC1;
+        private Inventory.DisplayTableUC displayTableUC1;
     }
 }
