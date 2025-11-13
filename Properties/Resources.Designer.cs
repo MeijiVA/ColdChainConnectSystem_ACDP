8bac7c5d563581173e1f001b0002b6101f8e17bc
MeijiVA <mizaelantonbautista@gmail.com>
2025-11-13 17:23:57 +0800

    Added CustomDatePicker

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -93,6 +93,26 @@ namespace ColdChainConnectSystem_ACDP.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        public static System.Drawing.Bitmap calendarDark {
+            get {
+                object obj = ResourceManager.GetObject("calendarDark", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        public static System.Drawing.Bitmap calendarWhite {
+            get {
+                object obj = ResourceManager.GetObject("calendarWhite", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         public static System.Drawing.Bitmap CCC_GrayLogo {
             get {
                 object obj = ResourceManager.GetObject("CCC_GrayLogo", resourceCulture);
