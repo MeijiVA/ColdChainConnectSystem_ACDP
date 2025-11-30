d1c3b39cf810cdce9e248d131b84c27420fd4d4d
Ezekiel Paguiligan <140354910+jankiel09@users.noreply.github.com>
2025-12-01 00:58:48 +0800

    Search term fixed.

--- AppForms/MainPanel/SearchUC.Designer.cs
+++ AppForms/MainPanel/SearchUC.Designer.cs
@@ -30,19 +30,19 @@
         {
             this.ofdImport = new System.Windows.Forms.OpenFileDialog();
             this.ofdExport = new System.Windows.Forms.SaveFileDialog();
-            this.cbxFilter = new ColdChainConnectSystem_ACDP.Materials.CustomComboBox();
+            this.layoutpanel = new System.Windows.Forms.FlowLayoutPanel();
             this.pnlSearch = new ColdChainConnectSystem_ACDP.Materials.CustomPanel();
             this.txtSearch = new System.Windows.Forms.TextBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.btnAddItem = new ColdChainConnectSystem_ACDP.Materials.CustomButton();
-            this.btnImport = new ColdChainConnectSystem_ACDP.Materials.CustomButton();
-            this.btnExport = new ColdChainConnectSystem_ACDP.Materials.CustomButton();
             this.fltrPanel = new ColdChainConnectSystem_ACDP.Materials.CustomPanel();
-            this.layoutpanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.cbxFilter = new ColdChainConnectSystem_ACDP.Materials.CustomComboBox();
+            this.btnAddItem = new ColdChainConnectSystem_ACDP.Materials.CustomButton();
+            this.btnExport = new ColdChainConnectSystem_ACDP.Materials.CustomButton();
+            this.btnImport = new ColdChainConnectSystem_ACDP.Materials.CustomButton();
+            this.layoutpanel.SuspendLayout();
             this.pnlSearch.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.fltrPanel.SuspendLayout();
-            this.layoutpanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // ofdImport
@@ -55,35 +55,27 @@
             this.ofdExport.InitialDirectory = "Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), \"D" +
     "ownloads\")";
             // 
-            // cbxFilter
+            // layoutpanel
             // 
-            this.cbxFilter.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(42)))), ((int)(((byte)(160)))));
-            this.cbxFilter.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(42)))), ((int)(((byte)(160)))));
-            this.cbxFilter.BorderRadius = 10;
-            this.cbxFilter.BorderSize = 1;
-            this.cbxFilter.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.cbxFilter.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDown;
-            this.cbxFilter.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.cbxFilter.ForeColor = System.Drawing.Color.White;
-            this.cbxFilter.IconColor = System.Drawing.Color.White;
-            this.cbxFilter.Items.AddRange(new object[] {
-            "Filter"});
-            this.cbxFilter.ListBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(228)))), ((int)(((byte)(245)))));
-            this.cbxFilter.ListTextColor = System.Drawing.Color.DimGray;
-            this.cbxFilter.Location = new System.Drawing.Point(0, 0);
-            this.cbxFilter.MinimumSize = new System.Drawing.Size(40, 30);
-            this.cbxFilter.Name = "cbxFilter";
-            this.cbxFilter.Padding = new System.Windows.Forms.Padding(1);
-            this.cbxFilter.Size = new System.Drawing.Size(100, 30);
-            this.cbxFilter.TabIndex = 5;
-            this.cbxFilter.Texts = "Filter";
-            this.cbxFilter.OnSelectedIndexChanged += new System.EventHandler(this.cbxFilter_OnSelectedIndexChanged);
-            this.cbxFilter.Load += new System.EventHandler(this.cbxFilter_Load);
+            this.layoutpanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(7)))), ((int)(((byte)(96)))));
+            this.layoutpanel.Controls.Add(this.pnlSearch);
+            this.layoutpanel.Controls.Add(this.fltrPanel);
+            this.layoutpanel.Controls.Add(this.btnAddItem);
+            this.layoutpanel.Controls.Add(this.btnExport);
+            this.layoutpanel.Controls.Add(this.btnImport);
+            this.layoutpanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.layoutpanel.ForeColor = System.Drawing.Color.Transparent;
+            this.layoutpanel.Location = new System.Drawing.Point(0, 0);
+            this.layoutpanel.Margin = new System.Windows.Forms.Padding(5);
+            this.layoutpanel.Name = "layoutpanel";
+            this.layoutpanel.Padding = new System.Windows.Forms.Padding(32, 7, 7, 7);
+            this.layoutpanel.Size = new System.Drawing.Size(755, 126);
+            this.layoutpanel.TabIndex = 7;
             // 
             // pnlSearch
             // 
-            this.pnlSearch.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(42)))), ((int)(((byte)(160)))));
-            this.pnlSearch.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(42)))), ((int)(((byte)(160)))));
+            this.pnlSearch.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(7)))), ((int)(((byte)(96)))));
+            this.pnlSearch.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(7)))), ((int)(((byte)(96)))));
             this.pnlSearch.BorderColor = System.Drawing.Color.White;
             this.pnlSearch.BorderRadius = 10;
             this.pnlSearch.BorderSize = 1;
@@ -99,7 +91,7 @@
             // 
             // txtSearch
             // 
-            this.txtSearch.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(42)))), ((int)(((byte)(160)))));
+            this.txtSearch.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(7)))), ((int)(((byte)(96)))));
             this.txtSearch.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.txtSearch.Dock = System.Windows.Forms.DockStyle.Fill;
             this.txtSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
@@ -127,6 +119,46 @@
             this.pictureBox1.TabIndex = 5;
             this.pictureBox1.TabStop = false;
             // 
+            // fltrPanel
+            // 
+            this.fltrPanel.BackColor = System.Drawing.Color.Transparent;
+            this.fltrPanel.BackgroundColor = System.Drawing.Color.Transparent;
+            this.fltrPanel.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.fltrPanel.BorderRadius = 5;
+            this.fltrPanel.BorderSize = 0;
+            this.fltrPanel.Controls.Add(this.cbxFilter);
+            this.fltrPanel.ForeColor = System.Drawing.Color.White;
+            this.fltrPanel.Location = new System.Drawing.Point(319, 10);
+            this.fltrPanel.Name = "fltrPanel";
+            this.fltrPanel.Size = new System.Drawing.Size(100, 30);
+            this.fltrPanel.TabIndex = 6;
+            this.fltrPanel.TextColor = System.Drawing.Color.White;
+            // 
+            // cbxFilter
+            // 
+            this.cbxFilter.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(42)))), ((int)(((byte)(160)))));
+            this.cbxFilter.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(42)))), ((int)(((byte)(160)))));
+            this.cbxFilter.BorderRadius = 10;
+            this.cbxFilter.BorderSize = 1;
+            this.cbxFilter.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.cbxFilter.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDown;
+            this.cbxFilter.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.cbxFilter.ForeColor = System.Drawing.Color.White;
+            this.cbxFilter.IconColor = System.Drawing.Color.White;
+            this.cbxFilter.Items.AddRange(new object[] {
+            "Filter"});
+            this.cbxFilter.ListBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(228)))), ((int)(((byte)(245)))));
+            this.cbxFilter.ListTextColor = System.Drawing.Color.DimGray;
+            this.cbxFilter.Location = new System.Drawing.Point(0, 0);
+            this.cbxFilter.MinimumSize = new System.Drawing.Size(40, 30);
+            this.cbxFilter.Name = "cbxFilter";
+            this.cbxFilter.Padding = new System.Windows.Forms.Padding(1);
+            this.cbxFilter.Size = new System.Drawing.Size(100, 30);
+            this.cbxFilter.TabIndex = 5;
+            this.cbxFilter.Texts = "Filter";
+            this.cbxFilter.OnSelectedIndexChanged += new System.EventHandler(this.cbxFilter_OnSelectedIndexChanged);
+            this.cbxFilter.Load += new System.EventHandler(this.cbxFilter_Load);
+            // 
             // btnAddItem
             // 
             this.btnAddItem.BackColor = System.Drawing.Color.Transparent;
@@ -145,35 +177,11 @@
             this.btnAddItem.GradientTextColor = System.Drawing.Color.Transparent;
             this.btnAddItem.Location = new System.Drawing.Point(425, 10);
             this.btnAddItem.Name = "btnAddItem";
-            this.btnAddItem.Size = new System.Drawing.Size(110, 30);
+            this.btnAddItem.Size = new System.Drawing.Size(115, 30);
             this.btnAddItem.TabIndex = 0;
             this.btnAddItem.TextColor = System.Drawing.Color.White;
             this.btnAddItem.UseVisualStyleBackColor = false;
             this.btnAddItem.Click += new System.EventHandler(this.btnAddItem_Click);
-            // 
-            // btnImport
-            // 
-            this.btnImport.BackColor = System.Drawing.Color.Transparent;
-            this.btnImport.BackgroundColor = System.Drawing.Color.Transparent;
-            this.btnImport.BackgroundImage = global::ColdChainConnectSystem_ACDP.Properties.Resources.import;
-            this.btnImport.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.btnImport.BorderColor = System.Drawing.Color.PaleVioletRed;
-            this.btnImport.BorderRadius = 10;
-            this.btnImport.BorderSize = 0;
-            this.btnImport.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnImport.FlatAppearance.BorderSize = 0;
-            this.btnImport.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnImport.ForeColor = System.Drawing.Color.White;
-            this.btnImport.GradientColor1 = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.btnImport.GradientColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.btnImport.GradientTextColor = System.Drawing.Color.Transparent;
-            this.btnImport.Location = new System.Drawing.Point(632, 10);
-            this.btnImport.Name = "btnImport";
-            this.btnImport.Size = new System.Drawing.Size(85, 30);
-            this.btnImport.TabIndex = 2;
-            this.btnImport.TextColor = System.Drawing.Color.White;
-            this.btnImport.UseVisualStyleBackColor = false;
-            this.btnImport.Click += new System.EventHandler(this.btnImport_Click);
             // 
             // btnExport
             // 
@@ -192,7 +200,7 @@
             this.btnExport.GradientColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
             this.btnExport.GradientTextColor = System.Drawing.Color.Transparent;
             this.btnExport.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnExport.Location = new System.Drawing.Point(541, 10);
+            this.btnExport.Location = new System.Drawing.Point(546, 10);
             this.btnExport.Name = "btnExport";
             this.btnExport.Size = new System.Drawing.Size(85, 30);
             this.btnExport.TabIndex = 1;
@@ -200,37 +208,29 @@
             this.btnExport.UseVisualStyleBackColor = false;
             this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
             // 
-            // fltrPanel
+            // btnImport
             // 
-            this.fltrPanel.BackColor = System.Drawing.Color.Transparent;
-            this.fltrPanel.BackgroundColor = System.Drawing.Color.Transparent;
-            this.fltrPanel.BorderColor = System.Drawing.Color.PaleVioletRed;
-            this.fltrPanel.BorderRadius = 5;
-            this.fltrPanel.BorderSize = 0;
-            this.fltrPanel.Controls.Add(this.cbxFilter);
-            this.fltrPanel.ForeColor = System.Drawing.Color.White;
-            this.fltrPanel.Location = new System.Drawing.Point(319, 10);
-            this.fltrPanel.Name = "fltrPanel";
-            this.fltrPanel.Size = new System.Drawing.Size(100, 30);
-            this.fltrPanel.TabIndex = 6;
-            this.fltrPanel.TextColor = System.Drawing.Color.White;
-            // 
-            // layoutpanel
-            // 
-            this.layoutpanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(7)))), ((int)(((byte)(96)))));
-            this.layoutpanel.Controls.Add(this.pnlSearch);
-            this.layoutpanel.Controls.Add(this.fltrPanel);
-            this.layoutpanel.Controls.Add(this.btnAddItem);
-            this.layoutpanel.Controls.Add(this.btnExport);
-            this.layoutpanel.Controls.Add(this.btnImport);
-            this.layoutpanel.Dock = System.Windows.Forms.DockStyle.Top;
-            this.layoutpanel.ForeColor = System.Drawing.Color.Transparent;
-            this.layoutpanel.Location = new System.Drawing.Point(0, 0);
-            this.layoutpanel.Margin = new System.Windows.Forms.Padding(5);
-            this.layoutpanel.Name = "layoutpanel";
-            this.layoutpanel.Padding = new System.Windows.Forms.Padding(32, 7, 7, 7);
-            this.layoutpanel.Size = new System.Drawing.Size(755, 126);
-            this.layoutpanel.TabIndex = 7;
+            this.btnImport.BackColor = System.Drawing.Color.Transparent;
+            this.btnImport.BackgroundColor = System.Drawing.Color.Transparent;
+            this.btnImport.BackgroundImage = global::ColdChainConnectSystem_ACDP.Properties.Resources.import;
+            this.btnImport.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnImport.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.btnImport.BorderRadius = 10;
+            this.btnImport.BorderSize = 0;
+            this.btnImport.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnImport.FlatAppearance.BorderSize = 0;
+            this.btnImport.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnImport.ForeColor = System.Drawing.Color.White;
+            this.btnImport.GradientColor1 = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btnImport.GradientColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btnImport.GradientTextColor = System.Drawing.Color.Transparent;
+            this.btnImport.Location = new System.Drawing.Point(637, 10);
+            this.btnImport.Name = "btnImport";
+            this.btnImport.Size = new System.Drawing.Size(85, 30);
+            this.btnImport.TabIndex = 2;
+            this.btnImport.TextColor = System.Drawing.Color.White;
+            this.btnImport.UseVisualStyleBackColor = false;
+            this.btnImport.Click += new System.EventHandler(this.btnImport_Click);
             // 
             // SearchUC
             // 
@@ -240,11 +240,11 @@
             this.Controls.Add(this.layoutpanel);
             this.Name = "SearchUC";
             this.Size = new System.Drawing.Size(755, 84);
+            this.layoutpanel.ResumeLayout(false);
             this.pnlSearch.ResumeLayout(false);
             this.pnlSearch.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.fltrPanel.ResumeLayout(false);
-            this.layoutpanel.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
