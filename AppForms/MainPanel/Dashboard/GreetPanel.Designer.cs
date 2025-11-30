e4f9a4a21bb54dfa3c93eec9f1ec18f937a470db
MeijiVA <mizaelantonbautista@gmail.com>
2025-11-30 17:23:16 +0800

    Inserted GreenUC into Panel

--- AppForms/MainPanel/Dashboard/GreetPanel.Designer.cs
+++ AppForms/MainPanel/Dashboard/GreetPanel.Designer.cs
@@ -32,6 +32,8 @@
             this.timelbl = new System.Windows.Forms.Label();
             this.daylbl = new System.Windows.Forms.Label();
             this.datelbl = new System.Windows.Forms.Label();
+            this.customPanel1 = new ColdChainConnectSystem_ACDP.Materials.CustomPanel();
+            this.customPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // greetlbl
@@ -39,8 +41,8 @@
             this.greetlbl.AutoSize = true;
             this.greetlbl.BackColor = System.Drawing.Color.Transparent;
             this.greetlbl.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10F);
-            this.greetlbl.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(7)))), ((int)(((byte)(96)))));
-            this.greetlbl.Location = new System.Drawing.Point(51, 17);
+            this.greetlbl.ForeColor = System.Drawing.Color.White;
+            this.greetlbl.Location = new System.Drawing.Point(43, 16);
             this.greetlbl.Name = "greetlbl";
             this.greetlbl.Size = new System.Drawing.Size(125, 16);
             this.greetlbl.TabIndex = 0;
@@ -50,8 +52,8 @@
             // 
             this.timelbl.BackColor = System.Drawing.Color.Transparent;
             this.timelbl.Font = new System.Drawing.Font("Arial Rounded MT Bold", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.timelbl.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(7)))), ((int)(((byte)(96)))));
-            this.timelbl.Location = new System.Drawing.Point(666, 5);
+            this.timelbl.ForeColor = System.Drawing.Color.White;
+            this.timelbl.Location = new System.Drawing.Point(647, 5);
             this.timelbl.Name = "timelbl";
             this.timelbl.Size = new System.Drawing.Size(133, 17);
             this.timelbl.TabIndex = 1;
@@ -60,8 +62,8 @@
             // 
             this.daylbl.BackColor = System.Drawing.Color.Transparent;
             this.daylbl.Font = new System.Drawing.Font("Arial Rounded MT Bold", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.daylbl.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(96)))));
-            this.daylbl.Location = new System.Drawing.Point(667, 17);
+            this.daylbl.ForeColor = System.Drawing.Color.White;
+            this.daylbl.Location = new System.Drawing.Point(648, 17);
             this.daylbl.Name = "daylbl";
             this.daylbl.Size = new System.Drawing.Size(132, 17);
             this.daylbl.TabIndex = 2;
@@ -70,25 +72,41 @@
             // 
             this.datelbl.BackColor = System.Drawing.Color.Transparent;
             this.datelbl.Font = new System.Drawing.Font("Arial Rounded MT Bold", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.datelbl.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(96)))));
-            this.datelbl.Location = new System.Drawing.Point(667, 30);
+            this.datelbl.ForeColor = System.Drawing.Color.White;
+            this.datelbl.Location = new System.Drawing.Point(648, 30);
             this.datelbl.Name = "datelbl";
             this.datelbl.Size = new System.Drawing.Size(131, 17);
             this.datelbl.TabIndex = 3;
+            // 
+            // customPanel1
+            // 
+            this.customPanel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(7)))), ((int)(((byte)(96)))));
+            this.customPanel1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(7)))), ((int)(((byte)(96)))));
+            this.customPanel1.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.customPanel1.BorderRadius = 15;
+            this.customPanel1.BorderSize = 0;
+            this.customPanel1.Controls.Add(this.datelbl);
+            this.customPanel1.Controls.Add(this.greetlbl);
+            this.customPanel1.Controls.Add(this.timelbl);
+            this.customPanel1.Controls.Add(this.daylbl);
+            this.customPanel1.ForeColor = System.Drawing.Color.White;
+            this.customPanel1.Location = new System.Drawing.Point(2, 3);
+            this.customPanel1.Name = "customPanel1";
+            this.customPanel1.Size = new System.Drawing.Size(792, 48);
+            this.customPanel1.TabIndex = 4;
+            this.customPanel1.TextColor = System.Drawing.Color.White;
             // 
             // GreetPanel
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.datelbl);
-            this.Controls.Add(this.daylbl);
-            this.Controls.Add(this.timelbl);
-            this.Controls.Add(this.greetlbl);
+            this.Controls.Add(this.customPanel1);
             this.Name = "GreetPanel";
             this.Size = new System.Drawing.Size(798, 51);
             this.Load += new System.EventHandler(this.GreetPanel_Load);
+            this.customPanel1.ResumeLayout(false);
+            this.customPanel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -98,5 +116,6 @@
         private System.Windows.Forms.Label timelbl;
         private System.Windows.Forms.Label daylbl;
         private System.Windows.Forms.Label datelbl;
+        private Materials.CustomPanel customPanel1;
     }
 }
